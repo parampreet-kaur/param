f9999a62fa6f522e31dfadb821532088b3297a35
parampreet-kaur <param6432@gmail.com>
2020-08-25 15:40:49 +0530

    partial submission of to-do app

---
+++ src/app.ts
@@ -0,0 +1,95 @@
+enum ToDoStatus {
+    active = "active",
+    done = "done",
+    deleted = "deleted"
+}
+
+class ToDo{
+    id: string;
+    description: string;
+    status: ToDoStatus;
+
+    constructor(id: string, description: string, status: ToDoStatus){
+       this.id = id;
+       this.description = description;
+       this.status = status; 
+    }
+
+    // getToDoInstance(id: number): ToDo{
+    //     return new ToDo(10, "des");
+    // }
+
+}
+
+class ToDoRender{
+    
+    renderToDo(toDo: ToDo){
+        let todoHtml = `
+        <div id="to-do-id" class="to-do-status row to-do-item w-100 m-0 d-flex min-height-48 pt-1">
+            <div class="col-sm-1 to-do-done p-0">
+                <input type="checkbox" name="toDoDone" class="w-100 m-0">
+            </div>
+            <div class="col-sm-10 to-do-text p-0 pb-1 px-0_5">
+                <span>
+                to-do-description
+                </span>
+            </div>
+            <div class="col-sm-1 remove-to-do p-0 pr-1">
+                <input type="button" class="button no-outline fr remove-button" name="removeToDo" value="X">
+            </div>
+        </div>
+        `
+        todoHtml.replace("to-do-id", toDo.id.toString());
+        todoHtml.replace("to-do-description", toDo.description);
+        todoHtml.replace("to-do-status", toDo.status.toString());
+        console.log(toDo);
+        //document.getElementById("to-do-items")?.appendChild(todoHtml);
+    }
+}
+
+var todoid: number = 1;
+
+var ToDoArray = [
+    new ToDo("to-do-1", 'First todo', ToDoStatus.active),
+    new ToDo("to-do-2", 'Second todo', ToDoStatus.deleted),
+    new ToDo("to-do-3", 'Third todo', ToDoStatus.active),
+    new ToDo("to-do-4", 'Fourth todo', ToDoStatus.done),
+];
+
+class ToDoOperation{
+
+    // getToDoInstance(id: number): ToDo{
+    //     return new ToDo(20, "des");
+    // }
+
+    getNextId(): number{
+        return todoid++;
+    }
+
+    addToDo(description: string){
+        var newToDo: ToDo = new ToDo("to-do-"+this.getNextId(), description, ToDoStatus.active);
+        
+        ToDoArray.push({
+            id : newToDo.id,
+            description : newToDo.description,
+            status : newToDo.status
+        });
+        var toDoRender: ToDoRender = new ToDoRender();
+        toDoRender.renderToDo(newToDo);
+        console.log(ToDoArray.toString());
+    }
+
+    renderCurrentToDos(){
+        if(ToDoArray.length > 0){
+            var toDoRender: ToDoRender = new ToDoRender();
+            for(var eachToDo of ToDoArray){
+                toDoRender.renderToDo(eachToDo);
+            }
+        }
+        console.log("Rendered current todos");
+    }
+}
+
+var op: ToDoOperation = new ToDoOperation();
+op.renderCurrentToDos();
+console.log("done");
